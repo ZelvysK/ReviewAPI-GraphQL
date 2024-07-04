8b9de4eef3267d5072b2c8119935334c8f37a8b8
Karolis Zelvys <koralbish@gmail.com>
2024-07-04 22:01:07 +0300

    Updated studio query

--- ReviewApp.API/Types/Queries/StudioQueries.cs
+++ ReviewApp.API/Types/Queries/StudioQueries.cs
@@ -1,16 +1,35 @@
-using System.Security.Claims;
 using HotChocolate.Authorization;
+using Microsoft.EntityFrameworkCore;
 using ReviewApp.API.Types.Base;
+using ReviewApp.API.Types.Enums;
 
 namespace ReviewApp.API.Types.Queries;
+
+public record GetStudiosInput(string? Term, StudioType? StudioType);
 
 [QueryType]
 public class StudioQueries
 {
     [Authorize]
-    [UseOffsetPaging]
-    public IQueryable<Studio> GetStudios(ReviewContext context)
+    [UseOffsetPaging(IncludeTotalCount = true)]
+    public IQueryable<Studio> GetStudios(ReviewContext context, GetStudiosInput input)
     {
-        return context.Studios.AsQueryable();
+        var query = context.Studios.AsNoTracking().AsQueryable();
+
+        if (string.IsNullOrWhiteSpace(input.Term) is false)
+        {
+            query = query.Where(x =>
+                x.Name.Contains(input.Term)
+                || x.Description != null && x.Description.Contains(input.Term)
+                || x.Founder.Contains(input.Term)
+            );
+        }
+
+        if (input.StudioType is not null)
+        {
+            query = query.Where(x => x.StudioType == input.StudioType);
+        }
+
+        return query;
     }
 }
